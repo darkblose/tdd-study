0d32c5d0ad977930ab8098003506ff3899d0ca5e
darkblose <darkblose@gmail.com>
2023-05-08 14:49:51 +0900

    JPA 적용하기

--- product-order-service/src/main/java/com/study/tdd/productorderservice/product/Product.java
+++ product-order-service/src/main/java/com/study/tdd/productorderservice/product/Product.java
@@ -1,14 +1,23 @@
 package com.study.tdd.productorderservice.product;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 class Product {
-    private final String name;
-    private final int price;
-    private final DiscountPolicy discountPolicy;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private int price;
+    private DiscountPolicy discountPolicy;
 
     public Product(final String name, final int price, final DiscountPolicy discountPolicy) {
         Assert.hasText(name, "상품명은 필수입니다.");
@@ -17,9 +26,5 @@ class Product {
         this.name = name;
         this.price = price;
         this.discountPolicy = discountPolicy;
-    }
-
-    public void assignId(final Long id) {
-        this.id = id;
     }
 }
