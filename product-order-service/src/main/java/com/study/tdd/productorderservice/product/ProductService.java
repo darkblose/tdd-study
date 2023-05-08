c0973674e7153d14c71484117e17ff9977342f43
darkblose <darkblose@gmail.com>
2023-05-08 14:33:13 +0900

    API 테스트로 전환하기

--- product-order-service/src/main/java/com/study/tdd/productorderservice/product/ProductService.java
+++ product-order-service/src/main/java/com/study/tdd/productorderservice/product/ProductService.java
@@ -1,17 +1,24 @@
 package com.study.tdd.productorderservice.product;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/products")
 @RequiredArgsConstructor
 class ProductService {
     private final ProductPort productPort;
 
-    @Transactional
-    public void addProduct(final AddProductRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
