bf81532d9987f0d2fd6c9cb20a4a61626322e7f9
darkblose <darkblose@gmail.com>
2023-05-08 14:04:24 +0900

    스프링부트 테스트로 전환하기

--- product-order-service/src/main/java/com/study/tdd/productorderservice/product/ProductAdapter.java
+++ product-order-service/src/main/java/com/study/tdd/productorderservice/product/ProductAdapter.java
@@ -1,7 +1,9 @@
 package com.study.tdd.productorderservice.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
 class ProductAdapter implements ProductPort {
     private final ProductRepository productRepository;
--- product-order-service/src/main/java/com/study/tdd/productorderservice/product/ProductService.java
+++ product-order-service/src/main/java/com/study/tdd/productorderservice/product/ProductService.java
@@ -1,11 +1,15 @@
 package com.study.tdd.productorderservice.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Component
 @RequiredArgsConstructor
 class ProductService {
     private final ProductPort productPort;
 
+    @Transactional
     public void addProduct(final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
         productPort.save(product);
