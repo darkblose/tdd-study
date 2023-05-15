44706667eb82b659687bb872e55bf00b9926f859
darkblose <darkblose@gmail.com>
2023-05-15 21:35:50 +0900

    상품 주문 API 개발 POJO 상품 주문 기능 구현하기

--- product-order-service/src/main/java/com/study/tdd/productorderservice/product/DiscountPolicy.java
+++ product-order-service/src/main/java/com/study/tdd/productorderservice/product/DiscountPolicy.java
@@ -1,5 +1,5 @@
 package com.study.tdd.productorderservice.product;
 
-enum DiscountPolicy {
+public enum DiscountPolicy {
     NONE
 }
--- product-order-service/src/main/java/com/study/tdd/productorderservice/product/Product.java
+++ product-order-service/src/main/java/com/study/tdd/productorderservice/product/Product.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Table(name = "products")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-class Product {
+public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
