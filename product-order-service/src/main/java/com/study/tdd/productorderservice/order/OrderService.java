547c816f463372b2db7e141b3d9f51fd4f4909d2
darkblose <darkblose@gmail.com>
2023-05-17 23:05:04 +0900

    상품 주문 API 개발 API 테스트로 전환하기

--- product-order-service/src/main/java/com/study/tdd/productorderservice/order/OrderService.java
+++ product-order-service/src/main/java/com/study/tdd/productorderservice/order/OrderService.java
@@ -2,18 +2,26 @@ package com.study.tdd.productorderservice.order;
 
 import com.study.tdd.productorderservice.product.Product;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/orders")
 @RequiredArgsConstructor
 class OrderService {
     private final OrderPort orderPort;
 
-    public void createOrder(final CreateOrderRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> createOrder(@RequestBody final CreateOrderRequest request) {
         final Product product = orderPort.getProductById(request.productId());
 
         final Order order = new Order(product, request.quantity());
 
         orderPort.save(order);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
